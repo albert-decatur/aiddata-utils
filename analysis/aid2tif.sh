c08459688ed99ea9d642f8b1665d5d43c5bde6b4
Albert Decatur <adecatur@aiddata.org>
2014-11-21 18:07:21 -0500

    get geom_id then join to geom

--- analysis/aid2tif.sh
+++ analysis/aid2tif.sh
@@ -6,11 +6,13 @@
 # inputs: postgres table with { projectid, financials column, precision code, lat, lng }, **and** allgeom table in postgis from GAUL boundaries with adm_level field (0|1|2) and geom field
 # output: raster with user's choice of x/y res with financials by pixel
 # NB: 
+#	if tables exist they must be dropped manually
 #	input aid table must have valid lng,lat, be wgs84, prec code must be {1,2,3,4,5,6,8}
 #	financials must refer to project level
 #	aid must fall within an allgeom to be considered when prec code is {3,4}, and to be clipped properly to a prec coce 2 (clipped by the adm0 it falls into)
 #	the following fields must in the inaid table:
 #		project_id,precision_code,latitude,longitude, financial field of user's choice
+#	assumes srid of allgeom is EPSG:4326
 # TODO: modify to allow for dasymetric output rasters, eg weight in-polygon split by an input raster like population, poverty, flood risk, ag output, etc
 # would be useful for comparing a series of scenarios and describing how the current distribution might be the result of several of these
 # example use: $0 m4r usd allgeom climate_cities /tmp/out.tif
@@ -25,7 +27,7 @@ rm $outrast 2>/dev/null
 function mk_intermediate_locs {
 	# make an intermediate table with financials_per_loc, assuming an even split of project funds between all project locs
 	echo "
-		drop table intermediate_locs;
+		--drop table intermediate_locs;
 		create table intermediate_locs as 
 			select 
 				$inaid.project_id,
@@ -74,36 +76,36 @@ function mk_prec_tables {
 			sed 's: or\s*$::g'
 		)
 	fi
+	adm=$2
 		echo "
-			drop table prec${prec_code_table};
+			--drop table prec${prec_code_table};
 			create table prec${prec_code_table} as 
 			select 
-				sum(tmp.financials_per_loc) as sum_financials_per_loc,
-				tmp.geom_id,
+				financials_per_loc,
 				a.geom 
 			from 
 				( 
 					select 
-						i.financials_per_loc,
+						sum(i.financials_per_loc) as financials_per_loc,
 						a.gid as geom_id 
 					from 
 						intermediate_locs as i,
 						allgeom as a 
 					where 
-						$prec_code_where
-						and a.adm_level = '${2}' 
+						( $prec_code_where ) 
+						and a.adm_level = '$adm' 
 						and st_within(i.geom,a.geom) 
+					group by 
+						a.gid 
 				) as tmp,
 				allgeom as a 
 			where 
-				tmp.geom_id = a.gid 
-			group by 
-				tmp.geom_id,a.geom
+				a.gid = tmp.geom_id
 			;"
 	}
 # mk prec code 1 table - just keep lat/lng
 echo "
-	drop table prec1;
+	--drop table prec1;
 	create table prec1 as 
 	select 
 		i.financials_per_loc,
@@ -116,31 +118,33 @@ echo "
 # mk prec code 2 table - buffer point by 25km and clip to adm0 it falls under
 # NB: hopefully will not need to st_makevalid on adm0
 echo "
-	drop table prec2;
+	--drop table prec2;
 	create table prec2 as 
 	select 
 		i.financials_per_loc,
+		-- clip the 25km buffer to the adm0 that the point lies in
 		st_intersection(
 			(
+				-- make the 25km bufer
 				(st_buffer(i.geom::geography,25000)::geometry)
 			),
 			st_makevalid(a.geom)
-		) as geom, 
+		) as geom 
 	from 
 		intermediate_locs as i,
 		allgeom as a 
 	where 
 		st_within(i.geom,a.geom) 
 		and a.adm_level = '0' 
-		and d.precision_code = '2'
+		and i.precision_code = '2'
 	;"
 # get geoms for prec 3
 by_adm 3 2
 # get geoms for prec 4
 by_adm 4 1
 # get geoms for prec 6 or 8
-by_adm "6 8" 1
+by_adm "6 8" 0
 }
 
-mk_intermediate_locs
-mk_prec_tables
+mk_intermediate_locs | psql $db
+mk_prec_tables | psql $db
