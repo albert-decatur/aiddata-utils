bdaeb11b647fa6b69e4d6a5b09519f32a7615368
Albert Decatur <adecatur@aiddata.org>
2014-11-24 16:06:19 -0500

    split by surface area. still need to do this for prec1

--- analysis/aid2tif.sh
+++ analysis/aid2tif.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # assign geoms to aiddata points and then rasterize and sum - assume distribution by first even split between locations and then even split along surface area
+# this holds true even for prec1 pixels - summed financials are divided by pixel-by-pixel surface area (good for any units, eg WGS84's degrees)
 # assign adm0,adm1,adm2,25 km buffered point, or just initial aid point based on precision code, lat, lng
 # assign financials based on input field of users choice - assumed to be at project level, distributed evenly across project locations
 # inputs: postgres table with { projectid, financials column, precision code, lat, lng }, **and** allgeom table in postgis from GAUL boundaries with adm_level field (0|1|2) and geom field
 # output: raster with user's choice of x/y res with financials by pixel
 # NB: 
-#	if tables exist they must be dropped manually
+#	if tables exist they must be d#ropped manually
 #	input aid table must have valid lng,lat, be wgs84, prec code must be {1,2,3,4,5,6,8}
 #	financials must refer to project level
 #	aid must fall within an allgeom to be considered when prec code is {3,4}, and to be clipped properly to a prec coce 2 (clipped by the adm0 it falls into)
@@ -17,7 +18,7 @@
 # would be useful for comparing a series of scenarios and describing how the current distribution might be the result of several of these
 # example use: $0 m4r usd allgeom climate_cities /tmp/out.tif
 
-inaid=m4r
+inaid=mini_m4r
 infinancials=usd
 ingeom=allgeom
 db=scratch
@@ -55,6 +56,9 @@ function mk_intermediate_locs {
 				;"
 }
 
+# make geometry for each precision code 1,2,3,4,5,6,8
+# assign GAUL2013 geom ADM0,1,2 where appropriate - aggregate to these geoms
+# divide financials by area
 function mk_prec_tables {
 # make function for geom by adm 0,1,or 2 - first arg is prec code second arg is adm_level from allgeom
 # note that this allows for multiple precision codes, good for prec code 6 and 8 which both refer to adm0
@@ -78,29 +82,37 @@ function mk_prec_tables {
 	fi
 	adm=$2
 		echo "
-			--drop table prec${prec_code_table};
+			drop table prec${prec_code_table};
 			create table prec${prec_code_table} as 
 			select 
-				financials_per_loc,
-				a.geom 
+				( financials_per_loc / ( st_area(geom::geography)/1000000 ) ) as financials_per_sqkm,
+				geom
 			from 
-				( 
+				(
 					select 
-						sum(i.financials_per_loc) as financials_per_loc,
-						a.gid as geom_id 
+						financials_per_loc,
+						a.geom 
 					from 
-						intermediate_locs as i,
+						( 
+							select 
+								sum(i.financials_per_loc) as financials_per_loc,
+								a.gid as geom_id 
+							from 
+								intermediate_locs as i,
+								allgeom as a 
+							where 
+								( $prec_code_where ) 
+								and a.adm_level = '$adm' 
+								and st_within(i.geom,a.geom) 
+							group by 
+								a.gid 
+						) as tmp,
 						allgeom as a 
 					where 
-						( $prec_code_where ) 
-						and a.adm_level = '$adm' 
-						and st_within(i.geom,a.geom) 
-					group by 
-						a.gid 
-				) as tmp,
-				allgeom as a 
-			where 
-				a.gid = tmp.geom_id
+						a.gid = tmp.geom_id 
+				) as a
+			;
+			alter table prec${prec_code_table} add column gid SERIAL
 			;"
 	}
 # mk prec code 1 table - just keep lat/lng
@@ -108,12 +120,15 @@ echo "
 	--drop table prec1;
 	create table prec1 as 
 	select 
+		i.project_id,
 		i.financials_per_loc,
 		i.geom 
 	from 
 		intermediate_locs as i 
 	where 
 		i.precision_code = '1'
+	;
+	alter table prec1 add column gid SERIAL
 	;"
 # mk prec code 2 table - buffer point by 25km and clip to adm0 it falls under
 # NB: hopefully will not need to st_makevalid on adm0
@@ -121,11 +136,12 @@ echo "
 	--drop table prec2;
 	create table prec2 as 
 	select
-		( tmp.financials_per_loc / st_area(tmp.geom::geography)/1000000 ) as financials_per_sqkm,
+		( tmp.financials_per_loc / ( st_area(tmp.geom::geography)/1000000 ) ) as financials_per_sqkm,
 		tmp.geom
 	from 
 		(
 			select
+				i.project_id,
 				i.financials_per_loc,
 				-- clip the 25km buffer to the adm0 that the point lies in
 				st_intersection(
@@ -143,14 +159,25 @@ echo "
 				and a.adm_level = '0' 
 				and i.precision_code = '2'
 		) as tmp
+	;
+	alter table prec2 add column gid SERIAL
 	;"
 # get geoms for prec 3
 by_adm 3 2
 # get geoms for prec 4
 by_adm 4 1
 # get geoms for prec 6 or 8
-by_adm "6 8" 0
+by_adm "5 6 8" 0
+}
+
+# make GIST index for each precision code table
+function mk_index { 
+	for table in prec{1..4} prec568
+	do
+		echo "create index i_${table} on $table using gist(geom);"
+	done
 }
 
 mk_intermediate_locs | psql $db
 mk_prec_tables | psql $db
+mk_index | psql $db
